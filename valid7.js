329903a39fbf90fe56cf96be9d20ed1444aba85e
priyanshuraj076 <120170370+priyanshuraj076@users.noreply.github.com>
2024-04-28 21:48:43 +0530

    Add files via upload

---
+++ valid7.js
@@ -0,0 +1,26 @@
+//Validation Code For Inputs
+
+var password = document.forms['form']['password'];
+
+var pass_error = document.getElementById('pass_error');
+
+password.addEventListener('input', pass_Verify);
+
+function validated() {
+    if (password.value !== "SABARMATIASHRAM") {
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    } else {
+        pass_error.style.display = "none";
+        window.location.href = "seventh.html";
+        return false; // Prevents form submission
+    }
+}
+
+function pass_Verify() {
+    if (password.value.length > 0) {
+        pass_error.style.display = "none";
+        return true;
+    }
+}
